253d997f3c4df595b778449f63a3ed9ed9373aad
Antismart <timbwamoses83@gmail.com>
2025-02-11 19:22:41 +0300

    create the BiconomySmartAccount

--- src/components/wallet.tsx
+++ src/components/wallet.tsx
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from "react";
 import SocialLogin from "@biconomy/web3-auth";
-import { ethers } from "ethers";
+import { ethers, providers } from "ethers";
 import { ChainId } from "@biconomy/core-types";
+import { BiconomySmartAccountV2, BiconomySmartAccountV2Config } from "@biconomy/account";
 
 export default function Wallet() {
   const sdkRef = useRef<SocialLogin | null>(null);
   const [interval, enableInterval] = useState<boolean>(false);
-
+  const [loading, setLoading] = useState(false);
+ 
   // login() function
 
   async function login() {
@@ -29,8 +31,45 @@ export default function Wallet() {
       //   setupSmartAccount();
     }
   }
-  
+
   // setupSmartAccount() function
+
+ 
+  const [, setProvider] = useState<providers.Web3Provider>();
+  const [smartAccount, setSmartAccount] = useState<BiconomySmartAccountV2>();
+
+  async function setupSmartAccount() {
+    try {
+      // If the SDK hasn't fully initialized, return early
+      if (!sdkRef.current?.provider) return;
+  
+      // Hide the wallet if currently open
+      sdkRef.current.hideWallet();
+  
+      // Start the loading indicator
+      setLoading(true);
+  
+      // Initialize the smart account
+      let web3Provider = new ethers.providers.Web3Provider(
+        sdkRef.current?.provider
+      );
+      setProvider(web3Provider);
+      const config: BiconomySmartAccountV2Config = {
+        signer: web3Provider.getSigner(),
+        chainId: ChainId.POLYGON_MUMBAI,
+        bundlerUrl: "",
+        biconomyPaymasterApiKey: "",
+      };
+      const smartAccount = await BiconomySmartAccountV2.create(config);
+  
+      // Save the smart account to a state variable
+      setSmartAccount(smartAccount);
+    } catch (e) {
+      console.error(e);
+    }
+  
+    setLoading(false);
+  }
 
   useEffect(() => {
     let configureLogin: NodeJS.Timeout | undefined;
