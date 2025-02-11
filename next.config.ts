cb260f6a70fb4c786d339c2cbfd146a7ea7ffd1c
Antismart <timbwamoses83@gmail.com>
2025-02-11 18:38:52 +0300

    configuring webpack

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,41 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+interface WebpackConfig {
+  resolve: {
+    fallback: {
+      fs: boolean;
+      net: boolean;
+      tls: boolean;
+    };
+  };
+}
+
+interface NextConfig {
+  reactStrictMode: boolean;
+  webpack: (config: WebpackConfig, options: { isServer: boolean }) => WebpackConfig;
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
